8abad8e65c4093336d5196e57165b367c610c224
udhbhav2012 <82303525+udhbhav2012@users.noreply.github.com>
2021-09-24 14:20:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+random_number = Math.floor((Math.random()*array_1.length)+1)
+
+Element_of_array = quick_draw_data_set[random_number]
+
+
+
+timer_counter = 0;
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
